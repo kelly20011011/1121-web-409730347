23c228cae50742fe6697432fc72d9b8b5d6d66bd
kellyyushin <409730347@gms.tku.edu.tw>
2023-11-08 01:24:53 +0800

    change demo sorse

--- demo/script_47.js
+++ demo/script_47.js
@@ -6,10 +6,25 @@ function showdemo (week){
             p.innerHTML=`<iframe src='./w01_47_intro/index.html'
             width="100%" height="100%" />`;
             break;
+        case 3:
+            p.innerHTML=`<iframe src='./w03_47_card_demo/index.html'
+            width="100%" height="100%" />`;
+            break;
+
+        case 4:
+            p.innerHTML=`<iframe src='./w04_47_card_demo/index.html'
+            width="100%" height="100%" />`;
+            break;
+        case 5:
+            p.innerHTML=`<iframe src='./w05_47_blog/index.html'
+            width="100%" height="100%" />`;
+            break;
+             
+             
         case 6:
-                p.innerHTML=`<iframe src='./w06_47_blog/index.html'
-                width="100%" height="100%" />`;
-                break;
+            p.innerHTML=`<iframe src='./w06_47_blog/index.html'
+            width="100%" height="100%" />`;
+            break;
                 
         case 7:
             p.innerHTML=`<iframe src='./index.html'width="100%" height="100%" />`;
