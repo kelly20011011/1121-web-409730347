c0e56f6a78e923a0e64c51de5dad71f5d616acf1
kellyyushin <409730347@gms.tku.edu.tw>
2023-11-26 09:33:59 +0800

    ### W11-P2: convert css of w1-intro and w3-card into scss, and insert them into p5_47.scss

--- demo/w11_47/p5_47.js
+++ demo/w11_47/p5_47.js
@@ -2,7 +2,7 @@ const navbar = document.querySelector('.nav');
 console.log('navbar', navbar);
 
 window.addEventListener('scroll', function(){
-    if( this.window.pageYOffset > 100){
+    if( this.window.scrollY > 100){
        navbar.classList.add('navbar-fixed');
     } else {
        navbar.classList.remove('navbar-fixed');
