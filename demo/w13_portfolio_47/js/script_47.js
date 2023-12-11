5c5e4a02946f98043b0f96a89fde7f279821515b
kellyyushin <409730347@gms.tku.edu.tw>
2023-12-11 21:08:40 +0800

    ### W13-P2: Show hero section with photo displayed

--- demo/w13_portfolio_47/js/script_47.js
+++ demo/w13_portfolio_47/js/script_47.js
@@ -1,8 +1,10 @@
 const navbar = document.querySelector('#nav');
 const date = document.querySelector('#date');
+
 const navBtn = document.querySelector('#nav-btn');
 const sidebar = document.querySelector('#sidebar');
 const closeBtn = document.querySelector('#close-btn');
+
 // add fixed class to navbar
 window.addEventListener('scroll', function () {
   if (window.scrollY > 80) {
@@ -11,11 +13,14 @@ window.addEventListener('scroll', function () {
     navbar.classList.remove('navbar-fixed');
   }
 });
+
 navBtn.addEventListener('click', function () {
   sidebar.classList.add('show-sidebar');
 });
+
 closeBtn.addEventListener('click', function () {
   sidebar.classList.remove('show-sidebar');
 });
+
 // set year
 date.innerHTML = new Date().getFullYear();
